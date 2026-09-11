7f84cf38d001d8220c1c0a3d0ecd7c4363ba84d4
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-09-11 15:00:52 +0200

    make processable on the gh CI runner

--- tests/test_memory_mapped_io.py
+++ tests/test_memory_mapped_io.py
@@ -1,5 +1,24 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import mmap
 import os
+from pathlib import Path
 
 import numpy as np
 
@@ -45,7 +64,9 @@ def get_memory_mapped_data(
 
 
 def test_memory_mapped_io():
-    file_path = f"{os.getcwd()}/tests/data/utils/memory_mapped_io.raw"
+    file_path = Path(f"{os.getcwd()}/tests/data/utils/memory_mapped_io.raw")
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+
     data_type_literal = "<f4"
     item_size = np.dtype(data_type_literal).itemsize
     assert item_size == 4
